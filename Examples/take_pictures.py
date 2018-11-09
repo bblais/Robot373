1fe6b4f1d8754ad796eb98028c525277976f3206
Brian Blais <bblais@gmail.com>
2018-11-09 09:15:48 -0500

    added take pictures script

---
+++ Examples/take_pictures.py
@@ -0,0 +1,24 @@
+from Robot373 import *
+import os
+
+if not os.path.exists('images'):
+    print('Making folder "images"')
+    os.mkdir('images')
+
+count=0
+while True:
+    x=raw_input('Hit enter to take a picture with no view.  Type any character and hit enter to take a picture with a view.')
+
+    if x:
+        view=True
+    else:
+        view=False
+
+    fname='images/test%d.jpg' % count
+    while os.path.exists(fname):
+        count+=1
+        fname='images/test%d.jpg' % count
+
+    take_picture('images/test%d.jpg' % count,view=view)
+    
+
