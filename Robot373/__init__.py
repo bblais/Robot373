f71b5a17709fe830f4486d13e49f21a190cd92ec
Brian Blais <bblais@gmail.com>
2019-12-02 19:34:57 -0500

    changed options on fswebcam

--- Robot373/__init__.py
+++ Robot373/__init__.py
@@ -1,4 +1,4 @@
 from .robot import *
 
-__version__='0.0.10'
+__version__='0.0.11'
 print("Version: ",__version__)
--- Robot373/robot.py
+++ Robot373/robot.py
@@ -225,7 +225,7 @@ from PIL import Image
 
 def take_picture(filename='picture.jpg',view=False):
 
-    a=os.system("fswebcam -S 20 --no-banner '%s'" % filename)
+    a=os.system("fswebcam -s brightness=100%% -r 1600x900 --no-banner current_board.jpg '%s'" % filename)
     print(a)
     if view:
         os.system('gpicview "%s" &' % filename)
